60d6bce22ae577f0193a78e0b37c3af4a451324d
Dima Pristupa <dima.pristupa@pm.me>
2022-12-22 12:32:38 +0200

    ✨ part00.rs: finish

--- src/part00.rs
+++ src/part00.rs
@@ -12,23 +12,20 @@ enum NumberOrNothing {
 
 // Observe how in Rust, the return type comes *after* the arguments.
 fn vec_min(vec: Vec<i32>) -> NumberOrNothing {
-    let mut min = NumberOrNothing::Nothing;
+    let mut min = Nothing;
 
     // Now we want to *iterate* over the list. Rust has some nice syntax for iterators:
     for el in vec {
         // So `el` is an element of the list. We need to update `min` accordingly, but how do we
         // get the current number in there? This is what pattern matching can do:
-        match min {
+        min = match min {
             // In this case (*arm*) of the `match`, `min` is currently nothing, so let's just make
             // it the number `el`.
-            NumberOrNothing::Nothing => {
-                unimplemented!()
-            }
+            Nothing => Number(el),
+
             // In this arm, `min` is currently the number `n`, so let's compute the new minimum and
             // store it.
-            NumberOrNothing::Number(n) => {
-                unimplemented!()
-            }
+            Number(n) => Number(min_i32(n, el)),
         }
     }
     // Finally, we return the result of the computation.
@@ -38,9 +35,9 @@ fn vec_min(vec: Vec<i32>) -> NumberOrNothing {
 // Now that we reduced the problem to computing the minimum of two integers, let's do that.
 fn min_i32(a: i32, b: i32) -> i32 {
     if a < b {
-        unimplemented!()
+        a
     } else {
-        unimplemented!()
+        b
     }
 }
 
@@ -56,13 +53,16 @@ use self::NumberOrNothing::{Nothing, Number};
 // a list of numbers, but for now, let's just hard-code something.
 
 fn read_vec() -> Vec<i32> {
-    unimplemented!()
+    vec![18, 5, 7, 1, 9, 27]
 }
 
 // Of course, we would also like to actually see the result of the computation, so we need to print the result.
 
 fn print_number_or_nothing(n: NumberOrNothing) {
-    unimplemented!()
+    match n {
+        Number(n) => println!("Number({n})"),
+        Nothing => println!("Nothing"),
+    }
 }
 
 // Putting it all together:
